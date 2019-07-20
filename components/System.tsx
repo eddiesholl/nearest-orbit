f700fc5802f2de06c6d8b13efa42d1854ca608a0
Eddie Sholl <esholl@zendesk.com>
2019-07-20 22:27:32 +1000

    Drawing with konva

--- components/System.tsx
+++ components/System.tsx
@@ -1,3 +1,13 @@
+import { Layer, Rect, Stage } from 'react-konva'
+import Konva from 'konva'
+
+const width = 500
+const height = 500
+
 export const System = () => (
-  <div>System</div>
+  <Stage width={width} height={height} >
+    <Layer>
+      <Rect width={width} height={height} fill={'#777'} />
+    </Layer>
+  </Stage>
 )
