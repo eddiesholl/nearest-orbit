f851dfbab9a15177d42bf4bac784063f3e8ad9c8
Eddie Sholl <esholl@zendesk.com>
2019-07-22 20:17:36 +1000

    Render distances as text

--- components/Data.tsx
+++ components/Data.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layer, Rect, Stage, Circle } from 'react-konva'
+import { Layer, Rect, Stage, Text } from 'react-konva'
 
 import { Frame } from '../model/types'
 
@@ -11,6 +11,9 @@ export const Data: React.SFC<DataProps> = ({ frame }) => (
   <Stage width={500} height={500} >
     <Layer>
       <Rect width={500} height={500} fill={'#555'} />
+      { frame.distances.map((d, ix) => (
+        <Text key={d.body.name} text={`${d.body.name}: ${d.au}`} x={20} y={20 + ix*20} fill={'#eee'} />
+      ))}
     </Layer>
   </Stage>
 )
