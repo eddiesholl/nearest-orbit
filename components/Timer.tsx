6a29d66c28733fb229b6ee644a6d3a3140e872f8
Eddie Sholl <esholl@zendesk.com>
2019-07-22 20:30:45 +1000

    Support variable timer speed

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -6,17 +6,21 @@ import { engine } from '../model/engine';
 export const Timer = ({ children }) => {
 
   const [time, setTime] = React.useState(0)
+  const [timeIncrement, setTimeIncrement] = React.useState(1000)
   const [isRunning, setIsRunning] = useState(true);
 
   useInterval(() => {
     setTime(time + 1)
-  }, isRunning ? 1000 : null)
+  }, isRunning ? timeIncrement : null)
 
   return (
     <React.Fragment>
       Timer { time }
-      <button onClick={() => setIsRunning(!isRunning)}>Toggle</button>
+      <button style={{ width: '100px'}} onClick={() => setIsRunning(!isRunning)}>
+        { isRunning ? 'Pause' : 'Run'}
+      </button>
       <input onChange={(event) => setTime(parseInt(event.target.value.toString()) || 0)} />
+      <input value={timeIncrement} onChange={(event) => setTimeIncrement(parseInt(event.target.value.toString()) || 1000)} />
       { children(engine(time)) }
     </React.Fragment>
   )
